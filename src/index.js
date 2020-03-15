30cad7ee9bc550a299a1f37680231f4b779d2b0b
Bekhruz Mirrakhimov <bexruz@gmail.com>
2020-03-15 22:11:14 +0500

    init:add task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let obj = { '(' : ')',
+  '{': '}',
+  '[': ']',
+  '|': '|',
+  '1': '2',
+  '3': '4',
+  '5': '6',
+  '7': '7',
+  '8': '8'}
+let stack = [];
+for(let i =0; i<str.length; i++){
+
+if(obj[stack[stack.length-1]] === str[i]) {
+stack.pop();
+} else {
+stack.push(str[i])
+}
+}
+if(stack.length > 0 ) {
+return false
+} else {
+return true
+}
 }
